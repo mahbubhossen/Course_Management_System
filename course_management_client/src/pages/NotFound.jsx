5ed862b0dd0b8187794d346d11785b217a0dd623
mahbubhossen <mahbubhossenpg@gamil.com>
2025-08-10 00:56:44 +0600

    fixed error page

--- course_management_client/src/pages/NotFound.jsx
+++ course_management_client/src/pages/NotFound.jsx
@@ -5,10 +5,10 @@ import useTitle from "../hooks/useTitle";
 const NotFound = () => {
   useTitle("404 Not Found");
   return (
-    <div className="h-screen flex flex-col items-center justify-center text-center p-4 max-w-xl mx-auto">
+    <div className="h-screen flex flex-col items-center justify-center text-center p-4 max-w-xl mx-auto text-base-content">
       <h1 className="text-6xl sm:text-7xl md:text-8xl font-bold text-red-600">404</h1>
-      <p className="text-xl sm:text-2xl mt-4 text-gray-700">Page Not Found</p>
-      <p className="mt-2 text-gray-500 px-2 sm:px-4 md:px-0">
+      <p className="text-xl sm:text-2xl mt-4">Page Not Found</p>
+      <p className="mt-2  px-2 sm:px-4 md:px-0">
         The page you are looking for doesn't exist or has been moved.
       </p>
       <Link
